06f9ba875c9ae36e65a8b403fb53b0551c3af0bb
thuanld <31129536+thuanld@users.noreply.github.com>
2017-08-18 17:38:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,14 +21,37 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+from random import randint
+
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello world1'
+    return 'Hello world from food for bot'
     
 @app.route('/webhook', methods=['POST', 'GET'])
 def webhook():
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    randSource = randint(0,9)
+    randSource = randint(0,1)
     yql_query = makeYqlQuery(randSource)
     if yql_query is None:
         return {}
@@ -45,27 +68,11 @@ def webhook():
 
 
 def makeYqlQuery(randSource): 
-    randPos = randint(1,6)
+    randPos = randint(1,3)
     if randSource == 0:
-        query = "select * from html where url=\"http://eva.vn/bep-eva-c162.html\" and xpath =\"/html/body/div[2]/div[4]/table/tbody/tr[1]/td[2]/div/div[2]/div[1]/a\""
+        query = "select * from htmlstring where url=\"http://eva.vn/bep-eva-c162.html\" and xpath =\"//*[@id="centerContent"]/div[1]/div[1]/div[4]/div[1]/a\""
     elif randSource == 1:
-        query = "select * from html where url=\"http://7monngonmoingay.net\" and xpath = \"/html/body/div[1]/div[2]/div[" + str(randPos) +"]/div[1]/a\""
-    elif randSource == 2:
-        query = "select * from html where url=\"http://www.phunutoday.vn/lam-me/\" and xpath =\"/html/body/main/div/div[1]/div[1]/div/section/div[1]/article[" + str(randPos) +"]/a\""
-    elif randSource == 3:
-        query = "select * from html where url=\"https://naungonmoingay.com/mon-ngon/\" and xpath =\"/html/body/div[3]/div[1]/div[3]/article[" + str(randPos) +"]/div[1]/a\""
-    elif randSource == 4:
-        query = "select * from html where url=\"http://us.24h.com.vn/mon-ngon-moi-ngay-c460e2552.html\" and xpath = \"/html/body/div[2]/div[3]/table/tbody/tr[1]/td[2]/div/div[2]/div[" + str(randPos + 1) +"]/span[1]/a\""
-    elif randSource == 5:
-        query = "select * from html where url=\"http://afamily.vn/an-ngon.chn\" and xpath = \"/html/body/form/div[2]/div[4]/div[2]/div[2]/div/div[2]/div/div[" + str(randPos) +"]/a\""
-    elif randSource == 6:
-        query = "select * from html where url=\"http://sotaynauan.com/chuyen-muc/mon-ngon-moi-ngay/\" and xpath = \"/html/body/div[2]/section/section/article[" + str(randPos) +"]/a\""
-    elif randSource == 7:
-        query = "select * from html where url=\"http://www.bepgiadinh.com/mon-ngon/\" and xpath = \"/html/body/div[1]/div/div/div[2]/div[1]/article/ul[" +  str(randint(3,8)) +"]/li[" +  str(randint(1,3)) +"]/a\""
-    elif randSource == 8:
-        query = "select * from html where url=\"http://feedy.vn/\" and xpath =\"/html/body/div[3]/div/div[2]/div[" +  str(randint(1,8)) +"]/div[1]/a\""
-    else:
-        query = "select * from html where url=\"http://kenh14.vn/made-by-me/kitchen.chn\" and xpath = \"/html/body/form/div[2]/div[2]/div[3]/div[2]/div/div/div[2]/div/div[3]/ul/li[" + str(randPos) +"]/div[1]/a\""
+        query = "select * from htmlstring where url=\"http://7monngonmoingay.net\" and xpath = \"//*[@id="content_box"]/article["+str(randPos)+"]/a\""    
     return query
 
 
@@ -92,22 +99,9 @@ def makeWebhookResult(randSource, data):
     
     if randSource == 0:
         url = "http://eva.vn" + a.get('href')
-    elif randSource == 1:
+    else randSource == 1:
         url = a.get('href')
-    elif randSource == 2:
-        url = "http://www.phunutoday.vn"+ a.get('href')
-    elif randSource == 3:
-        url = a.get('href')
-    elif randSource == 4:
-        url = a.get('href')
-    elif randSource == 5:
-        url = "http://afamily.vn"+ a.get('href')
-    elif randSource == 6:
-        url = a.get('href')
-    elif randSource == 7:
-        url = a.get('href')
-    else:
-        url = "http://kenh14.vn" + a.get('href')
+    
         
     facebook_message = {
         "attachment": {
